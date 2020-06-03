10af43a5e115ce166fc56f668897fb92250e0c01
Samuel Karatas <samuelkaratas@Samuels-MacBook-Pro.local>
2020-06-03 22:50:51 +0300

    connecting to database

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'samuelkaratas',
+    password : '',
+    database : 'wiml-db'
+  }
+});
+
 app.use(express.json());
 app.use(cors());
-
-const database = []
 
 //For testing
 const questions = [
@@ -131,40 +140,33 @@ const questions = [
 	}
 ]
 
-const servers = [];
-
 let qNumber = 0;
 app.get('/questionNumber/:sid', (req, res) => {
 	//Selects a random number
 	//Filters the database according to the server id
 	//Changes the qid(question id) of every player that is in that server
 	qNumber = Math.round(Math.random() * questions.length);
-	const returningUserArray = database.filter(user => user.sid == req.params.sid);
-	for(let i = 0; i < returningUserArray.length; i++){
-		returningUserArray[i].qid = qNumber;
-	}
-	res.json(returningUserArray)
+	db('servers')
+	  .where({ sid: req.params.sid })
+	  .update({ qid: qNumber }, ['qid'])
+	  .then(data => {
+	  	res.json(questions[data[0].qid-1].question);
+	  }).catch(err => res.status(400).json('unable to update question number'))
 })
 
 app.get('/question/:sid', (req, res) => {
 	//returns the scores to zero
 	//sends the question according to the qid
-	const returningUserArray = database.filter(user => user.sid == req.params.sid);
-	for(let i = 0; i < returningUserArray.length; i++){
-		returningUserArray[i].score = 0;
-	}
-	res.send(questions[returningUserArray[0].qid-1]);
-})
-
-app.get('/', (req, res) => {
-	//returns all the users in the database
-	res.send(database)
+	db.select('qid').from('servers').where({ sid: req.params.sid}).then(data => {
+		res.json(questions[data[0].qid-1].question);
+	}).catch(err => res.status(400).json('unable to get question'))
 })
 
 app.get('/profile/:sid', (req, res) => {
 	//returns all the users with the same sid in the url
-	const returningUserArray = database.filter(user => user.sid == req.params.sid);
-	res.send(returningUserArray)
+	db.select('*').from('users').where({ sid: req.params.sid }).then(data => {
+		res.json(data);
+	}).catch(err => res.status(400).json('unable to get users'))
 })
 
 app.get('/leaderboard/:sid', (req, res) => {
@@ -172,99 +174,110 @@ app.get('/leaderboard/:sid', (req, res) => {
 	//then gets the score and stores in an array. then sorts it
 	//then pushes in the name array with the name and score
 	//NEEDS OPTIMIZATION
-	const returningUserArray = database.filter(user => user.sid == req.params.sid);
-	const scoreArray = returningUserArray.map(user => {
-		return(user.score)
-	});
-	scoreArray.sort((a,b) => b-a);
-	const scoreArray2 = scoreArray.filter(score => score > 0);
-	let nameArray = [];
-	for(let i = 0; i < scoreArray2.length; i++){
-		for(let j = 0; j < returningUserArray.length; j++){
-			if(scoreArray2[i] == returningUserArray[j].score){
-				nameArray.push(returningUserArray[j].name + ' = ' + returningUserArray[j].score);
-			}
-		}
-	}
-	uniqueArray = nameArray.filter((item, pos) => {
-	    return (nameArray.indexOf(item) == pos);
-	})
-	res.send(uniqueArray);
-})
-
-app.get('/profile/:sid/:id', (req, res) => {
-	//checks the users with the same sid in the url and returns
-	//the user with the same id in the second param in the url
-	const returningUserArray = database.filter(user => {
-		return (user.sid == req.params.sid && user.id == req.params.id);
-	});
-	res.send(returningUserArray[0].name)
+	db.select('selectedname', db.raw('COUNT(id)'))
+		.from('users')
+		.where({ sid: req.params.sid })
+		.groupBy('selectedname')
+		.orderBy('count', 'desc')
+		.then(data => {
+			res.json(data);
+		}).catch(err => res.status(400).json('unable to get leaderboard'))
 })
 
 app.put('/profile/:sid/:id', (req, res) => {
 	//Gets the user with the url the gets the selected name of that user
 	//then gets the user with the same selected name then increments the score
-	const returningUserArray = database.filter(user => {
-		return (user.sid == req.params.sid && user.id == req.params.id);
-	})
-	returningUserArray[0].selectedName = req.body.selectedName;
-	const returningUserArray2 = database.filter(user => {
-		return (user.name === returningUserArray[0].selectedName);
-	})
-	res.json(returningUserArray2[0].score++)
+	db('users')
+	  .where({ 
+		sid: req.params.sid,
+		id: req.params.id
+	  })
+	  .update({ selectedname: req.body.selectedname }, ['selectedname'])
+	  .then(data => {
+	  	db('users')
+	  	  .returning('score')
+		  .where({ 
+		  	sid: req.params.sid,
+		  	name: data[0].selectedname
+		  })
+		  .increment('score')
+		  .then(data => {
+		  	res.json(data);
+		  }).catch(err => res.status(400).json('unable to update the score'))
+	  }).catch(err => res.status(400).json('unable to add the selected name'))
 })
 
 app.put('/update/:sid', (req, res) => {
 	//only the admin can access
 	//changes the is approved 
-	const returningUserArray = database.filter(user => {
-		return (user.sid == req.params.sid);
-	})
-	for(let i = 0; i < returningUserArray.length; i++){
-		returningUserArray[i].isApproved = req.body.isApproved;
-	}
-	res.json(returningUserArray)
+	db('users')
+	  .where({ sid: req.params.sid })
+	  .update({ isapproved: req.body.isapproved }, ['id', 'isapproved'])
+	  .then(data => {
+	  	res.json(data);
+	  }).catch(err => res.status(400).json('unable to update is approved'))
 })
 
 app.get('/getupdate/:sid/:id', (req, res) => {
 	//returns the is approved state
-	const returningUserArray = database.filter(user => {
-		return (user.sid == req.params.sid && user.id == req.params.id);
-	})
-	res.json(returningUserArray[0].isApproved)
+	const { sid, id } = req.params;
+	db.select('isapproved').from('users').where({
+		id: id
+	}).then(data => {
+		res.json(data[0].isapproved);
+	}).catch(err => res.status(400).json('unable to get is approved'))
 })
 
 app.put('/getupdate/:sid/:id', (req, res) => {
 	//changes the isapproved to false
-	const returningUserArray = database.filter(user => {
-		return (user.sid == req.params.sid && user.id == req.params.id);
-	})
-	returningUserArray[0].isApproved = false;
-	res.json(returningUserArray[0].isApproved)
+	db('users')
+	  .where({ id: req.params.id })
+	  .update({ isapproved: false }, ['id', 'isapproved'])
+	  .then(data => {
+	  	res.json(data[0].isapproved);
+	  }).catch(err => res.status(400).json('unable to update is approved to false'))
 })
 
-let id1 = 9;
+//let id1 = 9;
 app.post('/signin', (req, res) => {
-	database.push({
-		id: id1,
-		qid: 1,
-		sid: req.body.sid,
-		name: req.body.name,
-		score: 0,
-		selectedName: "",
-		isApproved: false
-	},)
-	id1++;
-	res.send(database[database.length - 1])
+	const { sid, name } = req.body;
+	db('users').returning('*').insert({
+		name: name,
+		sid: sid
+	}).then(user => {
+		res.json(user[0])
+	}).catch(err => res.status(400).json('unable to signin'))
 })
 
 app.post('/createParty', (req, res) => {
-	servers.push(req.body.sid)
-	res.send(servers)
+	db('servers').returning('*').insert({
+		sid: req.body.sid
+	}).then(data => {
+		res.json(data);
+	}).catch(err => res.status(400).json('error creating party'))
 })
 
 app.get('/getParty', (req, res) => {
-	res.send(servers)
+	db.select('*').from('servers').then(data => {
+		res.json(data);
+	}).catch(err => res.status(400).json('unable to get party'))
+})
+
+app.post('/signout', (req, res) => {
+	db('users').where({
+		id: req.body.id
+	}).del().then(data => res.json(data))
+})
+
+app.post('/deleteServer', (req, res) => {
+	//only admin can delete server
+	db('servers').where({
+		sid: req.body.sid
+	}).del().then(data => {
+		db('users').where({
+			sid: req.body.sid
+		}).del().then(data2 => res.json(data2))
+	})
 })
 
 app.listen(3000, () => {
